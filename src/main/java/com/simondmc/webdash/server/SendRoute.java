6da0b32e2711ddd1940b991f9dab6ce4cc3359ac
SimonDMC <simondmcplayer@gmail.com>
2023-01-21 12:42:05 +0100

    Change /send route to POST

--- src/main/java/com/simondmc/webdash/server/SendRoute.java
+++ src/main/java/com/simondmc/webdash/server/SendRoute.java
@@ -5,7 +5,9 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.bukkit.Bukkit;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URI;
 import java.util.ArrayList;
@@ -15,9 +17,10 @@ public class SendRoute implements HttpHandler {
 
     @Override
     public void handle(HttpExchange he) throws IOException {
-        // parse request
-        URI requestedUri = he.getRequestURI();
-        String query = requestedUri.getRawQuery();
+        // parse POST request
+        InputStreamReader isr = new InputStreamReader(he.getRequestBody(), "utf-8");
+        BufferedReader br = new BufferedReader(isr);
+        String query = br.readLine();
         // parse query
         List<String> params = new ArrayList<>(List.of(query.split("&")));
 
