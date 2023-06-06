7200d85601bb67298dde4e0ce15a3949847f5b80
SimonDMCPlayer <46278840+SimonDMCPlayer@users.noreply.github.com>
2023-06-06 17:38:42 +0200

    Refactor main command class

--- src/main/java/com/simondmc/webdash/command/WebDashCommand.java
+++ src/main/java/com/simondmc/webdash/command/WebDashCommand.java
@@ -26,46 +26,23 @@ public class WebDashCommand implements CommandExecutor {
             }
 
             String subcommand = args[0].toLowerCase();
-            WebDashSubcommand executor = null;
+            WebDashSubcommand executor;
 
-            if (subcommand.equals("add") || subcommand.equals("a")) {
-                executor = new WebDashAddSubcommand();
+            switch (subcommand) {
+                case "add", "a" -> executor = new WebDashAddSubcommand();
+                case "remove", "delete", "rm", "del" -> executor = new WebDashRemoveSubcommand();
+                case "list", "ls" -> executor = new WebDashListSubcommand();
+                case "link", "url" -> executor = new WebDashLinkSubcommand();
+                case "restart", "reload" -> executor = new WebDashRestartSubcommand();
+                case "key" -> executor = new WebDashKeySubcommand();
+                case "on", "enable" -> executor = new WebDashOnSubcommand();
+                case "off", "disable" -> executor = new WebDashOffSubcommand();
+                default -> {
+                    return false;
+                }
             }
-
-            if (subcommand.equals("remove") || subcommand.equals("delete") || subcommand.equals("rm") || subcommand.equals("del")) {
-                executor = new WebDashRemoveSubcommand();
-            }
-
-            if (subcommand.equals("list") || subcommand.equals("ls")) {
-                executor = new WebDashListSubcommand();
-            }
-
-            if (subcommand.equals("link") || subcommand.equals("url")) {
-                executor = new WebDashLinkSubcommand();
-            }
-
-            if (subcommand.equals("restart") || subcommand.equals("reload")) {
-                executor = new WebDashRestartSubcommand();
-            }
-
-            if (subcommand.equals("key")) {
-                executor = new WebDashKeySubcommand();
-            }
-
-            if (subcommand.equals("on") || subcommand.equals("enable")) {
-                executor = new WebDashOnSubcommand();
-            }
-
-            if (subcommand.equals("off") || subcommand.equals("disable")) {
-                executor = new WebDashOffSubcommand();
-            }
-
-            if (executor == null) {
-                return false;
-            } else {
-                executor.execute(sender, args);
-                return true;
-            }
+            executor.execute(sender, args);
+            return true;
         }
         return false;
     }
