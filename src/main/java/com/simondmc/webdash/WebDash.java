bccf06825aa19e9e10159f9bdc03bef0452efe61
SimonDMCPlayer <46278840+SimonDMCPlayer@users.noreply.github.com>
2023-06-22 21:41:27 +0200

    Clean up main class

--- src/main/java/com/simondmc/webdash/WebDash.java
+++ src/main/java/com/simondmc/webdash/WebDash.java
@@ -3,8 +3,8 @@ package com.simondmc.webdash;
 import com.simondmc.webdash.command.WebDashCommand;
 import com.simondmc.webdash.command.WebDashTabCompleter;
 import com.simondmc.webdash.config.*;
-import com.simondmc.webdash.websocket.WSSHandler;
 import com.simondmc.webdash.server.WebServer;
+import com.simondmc.webdash.websocket.WSSHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
@@ -19,33 +19,15 @@ public final class WebDash extends JavaPlugin {
         plugin = this;
         logger = getLogger();
 
-        // register commands
+        // register command
         getCommand("webdash").setExecutor(new WebDashCommand());
-        getCommand("wd").setExecutor(new WebDashCommand());
         getCommand("webdash").setTabCompleter(new WebDashTabCompleter());
-        getCommand("wd").setTabCompleter(new WebDashTabCompleter());
 
-        // copy over necessary images
+        // copy over necessary images from resources to plugin folder
         saveImages();
 
-        // init main config
-        Configs.add("config.yml");
-
-        // init routes config
-        // this config stores buttons/routes added by players
-        RoutesConfig.init();
-
-        // init data config
-        // this config stores key data and on/off status
-        DataConfig.init();
-
-        // init messages config
-        // this config is used as a language file
-        MessagesConfig.init();
-
-        // init version config
-        // this config stores the current version of the plugin for access by info message
-        VersionConfig.init();
+        // initialize config files
+        initConfigs();
 
         // start web server
         WebServer.start();
@@ -66,5 +48,22 @@ public final class WebDash extends JavaPlugin {
         saveResource("dash/favicon.ico", true);
         saveResource("dash/icon-192.png", true);
         saveResource("dash/icon-512.png", true);
+    }
+
+    void initConfigs() {
+        // user config
+        Configs.add("config.yml");
+
+        // routes.yml stores buttons/routes added by players
+        RoutesConfig.init();
+
+        // data.yml stores key data and on/off status
+        DataConfig.init();
+
+        // messages.yml is used as a language file
+        MessagesConfig.init();
+
+        // version.yml stores the current version of the plugin for access by info message
+        VersionConfig.init();
     }
 }
